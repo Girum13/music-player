69ffe47ddfa30b475cd04f45c2b71fe28610ed08
Girum13 <tsegugirum@gmail.com>
2022-06-16 06:31:22 -0700

    adding main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/screen/music_app.dart';
+import '../screen/about.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MusicApp(),
+        routes: {
+          //.routeName: (_) => const DeveloperScreen(),
+          AboutScreen.routeName: (_) => const AboutScreen(),
+        });
+  }
+}
